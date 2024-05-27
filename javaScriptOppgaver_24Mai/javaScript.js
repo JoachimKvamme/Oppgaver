5640b54423e79910fc416d2ee46e71d3686bed26
JoachimKvamme <joachim.kvamme@gmail.com>
2024-05-27 12:56:23 +0200

    Ferdig med oppgavene fra fredag 24. mai.

--- javaScriptOppgaver_24Mai/javaScript.js
+++ javaScriptOppgaver_24Mai/javaScript.js
@@ -24,8 +24,10 @@ function loggInn() {
 
     if (brukernavn == "test_person" && passord == "1234512345") {
         document.getElementById("innloggingsresultat").innerHTML = `Hurra! Velkommen ${brukernavn}, du klarte å logge inn.`
+        document.getElementById("innloggingsresultat").style.backgroundColor = "green";
     } else {
         document.getElementById("innloggingsresultat").innerHTML = `Her har noe gått galt. Enten eksisterer ikke brukernavnet ${brukernavn}, eller passordet er feil.`
+        document.getElementById("innloggingsresultat").style.backgroundColor = "red";
     }
 }
 
@@ -47,5 +49,34 @@ function epostGenerator() {
 }
 
 // Oppgave 4
+
+function pris() {
+    reisestrekning = document.getElementById("reisedistanse").value;
+    alder = document.getElementById("alder").value;
+
+    if (alder < 13) {
+        let pris = (reisestrekning * 3.52) * 0.5;
+        document.getElementById("billettpris").innerHTML = `Fergebilletten koster ${pris}, når turen er på ${reisestrekning} kilometer, for en reisende på ${alder} år.`
+    }
+    else if (alder > 66) {
+        let pris = ((reisestrekning * 3.52) * 0.8);
+        document.getElementById("billettpris").innerHTML = `Fergebilletten koster ${pris}, når turen er på ${reisestrekning} kilometer, for en reisende på ${alder} år.`
+    }
+    else if (alder > 12) {
+        let pris = (reisestrekning * 3.52);
+        document.getElementById("billettpris").innerHTML = `Fergebilletten koster ${pris} kroner, når turen er på ${reisestrekning} kilometer, for en reisende på ${alder} år.`
+    }
+}
+function prisFlere() {
+
+        let strekning = document.getElementById("reisestrekningFlere").value;
+        let reisendeBarn = document.getElementById("reisendeBarn").value;
+        let reisendeVoksne = document.getElementById("reisendeVoksne").value;
+        let reisendeGamle = document.getElementById("reisendeGamle").value;
+
+        let pris = (strekning * (reisendeVoksne * 3.52)) + (strekning * (reisendeBarn * 3.52 * 0.5)) + (strekning * (reisendeGamle * 3.52 * 0.8))
+        document.getElementById("billettprisFlere").innerHTML = `For ${reisendeBarn} barn, ${reisendeVoksne} voksne og ${reisendeGamle} pensjonister, koster en tur på ${strekning} kilometer ${pris} kroner.`
+    }
+
 
 
