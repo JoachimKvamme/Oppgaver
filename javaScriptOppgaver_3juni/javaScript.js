16ccd0d8c8bcc14dd987000d3b1ae69bcc0bb8d9
JoachimKvamme <joachim.kvamme@gmail.com>
2024-06-05 11:28:57 +0200

    Gjorde ferdig oppgavesettet fra 3. juni.

--- javaScriptOppgaver_3juni/javaScript.js
+++ javaScriptOppgaver_3juni/javaScript.js
@@ -38,6 +38,8 @@ function tippingTo() {
 // Oppgave 4
 
 function kmiKalkulator() {
+    // Henter vekt og høyde som brukeren har skrevet inn fra et HTML-dokument, og regner ut kroppsmasseindeks. 
+   
     let vekt = Number(document.getElementById("vekt").value)
     let høyde = Number(document.getElementById("høyde").value)
     høyde = (høyde / 100)
@@ -54,4 +56,69 @@ function kmiKalkulator() {
     }
 
 
+}
+
+// Oppgave 5
+
+function dato() {
+    let dagStreng = parseInt(document.getElementById("datoStreng").value.slice(0, 2), 10)
+    let mmStreng = document.getElementById("datoStreng").value.slice(3)
+    
+    switch (mmStreng) {
+        case "01":
+            document.getElementById("dato").innerHTML = `${dagStreng}. januar`
+            break
+        case "02":
+            document.getElementById("dato").innerHTML = `${dagStreng}. februar`
+            break
+        case "03":
+            document.getElementById("dato").innerHTML = `${dagStreng}. mars`
+            break
+        case "04":
+            document.getElementById("dato").innerHTML = `${dagStreng}. april`
+            break
+        case "05":
+            document.getElementById("dato").innerHTML = `${dagStreng}. mai`
+            break
+        case "06":
+            document.getElementById("dato").innerHTML = `${dagStreng}. juni`
+            break
+        case "07":
+            document.getElementById("dato").innerHTML = `${dagStreng}. juli`
+            break
+        case "08":
+            document.getElementById("dato").innerHTML = `${dagStreng}. august`
+            break
+        case "09":
+            document.getElementById("dato").innerHTML = `${dagStreng}. september`
+            break
+        case "10":
+            document.getElementById("dato").innerHTML = `${oktober}. oktober`
+            break
+        case "11":
+            document.getElementById("dato").innerHTML = `${dagStreng}. november`
+            break
+        case "12":
+            document.getElementById("dato").innerHTML = `${dagStreng}. desember`
+            break
+        default:
+            document.getElementById("dato").innerHTML = `Du har tastet inn en ugyldig dato, eller en dato i et ugyldig format.`
+    }
+    
+    document.getElementById("datoStreng").focus()
+
+}
+
+// Oppgave 6
+
+function skuddår() {
+    let år = document.getElementById("år").value
+
+    if ((år % 4 == 0) && !(år % 100 == 0) || (år % 400 == 0)) {
+        document.getElementById("resultatSkuddår").innerHTML = `${år} er et skuddår.`
+    } else {
+        document.getElementById("resultatSkuddår").innerHTML = `${år} er ikke et skuddår.`
+    }
+    
+    document.getElementById("år").focus()
 }
